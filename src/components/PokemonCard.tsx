af04c66abd0f3ad191b07d3eb3df303c79b30d24
unknown <conalesjames@gmail.com>
2025-12-22 23:27:07 +0845

    Minor Enhancement

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,6 +1,6 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { myGlobalContext } from "@/GlobalContext";
-import React, { useState } from "react";
+
+import { useState } from "react";
 interface PokemonCardProps {
   name: string;
   url: string;
